abc8ef9f95555ba43b4de8900458d2e4c68c4028
FlaxCube <shivamashokdhage6@gmail.com>
2024-06-19 10:53:52 +0530

    Using Bind Approach for Systemless Hosts

--- Portal/BLScripts/SYSH.sh
+++ Portal/BLScripts/SYSH.sh
@@ -2,17 +2,14 @@
 if [ ! -d "/data/adb/magisk" ]; then
     if [ "$(READ "BLS4" "$BLC")" = "OFF" ]; then
       rm -rf "$DB/system"
-      rm -rf "$DB/worker"
       echo "\e[01;31m!! Reboot Required \e[0m"
       sleep 2
     elif [ ! "$(getprop sys.boot_completed)" == "1" ]; then
-mount -t overlay -o lowerdir=/system/etc,upperdir=/data/INDRA/system/etc,workdir=/data/INDRA/worker SYSHOST /system/etc
+      mount --bind "$DB/system/etc/hosts" /system/etc/hosts
     else
       mkdir -p "$DB/system/etc"
       cp -af /system/etc/hosts "$DB/system/etc/hosts"
-     chcon -r u:object_r:system_file:s0 "$DB/system"
-     chmod 644 "$DB/system/etc/hosts"
-     mkdir -p "$DB/worker"
+      chmod 644 "$DB/system/etc/hosts"
       echo "\e[01;31m!! Reboot Required \e[0m"
       sleep 2
     fi
